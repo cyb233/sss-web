262a5fceb866dcba72ce20230dcc0484fdccad79
cyb233 <1252237203@qq.com>
2022-02-03 20:09:10 +0800

    cors处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,7 +6,7 @@ import { getToken } from '@/utils/auth'
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  withCredentials: false, // send cookies when cross-domain requests
+  withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
 
@@ -48,7 +48,7 @@ service.interceptors.response.use(
     const res = response.data
 
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 200) {
+    if (res.code !== 20000) {
       Message({
         message: res.message || 'Error',
         type: 'error',
