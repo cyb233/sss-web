21d25a3aa4838d83054f921bd4770e07847cc7a0
Schwi <39186981+cyb233@users.noreply.github.com>
2022-02-09 15:38:10 +0800

    Update index.vue

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,15 +6,15 @@
         <h3 class="title">Login Form</h3>
       </div>
 
-      <el-form-item prop="username">
+      <el-form-item prop="loginCode">
         <span class="svg-container">
           <svg-icon icon-class="user" />
         </span>
         <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
+          ref="loginCode"
+          v-model="loginForm.loginCode"
+          placeholder="loginCode"
+          name="loginCode"
           type="text"
           tabindex="1"
           auto-complete="on"
@@ -44,7 +44,7 @@
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
 
       <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
+        <span style="margin-right:20px;">loginCode: admin</span>
         <span> password: any</span>
       </div>
 
@@ -53,14 +53,14 @@
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
+import { validLoginCode } from '@/utils/validate'
 
 export default {
   name: 'Login',
   data() {
-    const validateUsername = (rule, value, callback) => {
+    const validateLoginCode = (rule, value, callback) => {
       callback()
-      if (!validUsername(value)) {
+      if (!validLoginCode(value)) {
         callback(new Error('Please enter the correct user name'))
       } else {
         callback()
@@ -75,11 +75,11 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin',
+        loginCode: 'admin',
         password: '111111'
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
+        loginCode: [{ required: true, trigger: 'blur', validator: validateLoginCode }],
         password: [{ required: true, trigger: 'blur', validator: validatePassword }]
       },
       loading: false,
